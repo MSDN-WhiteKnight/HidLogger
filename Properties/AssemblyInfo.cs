862475feae8460fa3bd6e94b86729274bd47dbf0
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2018-05-14 01:03:18 +0500

    Add files via upload

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("HidLogger")]
+[assembly: AssemblyDescription("HID USB TraceEvent logger sample")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Private person")]
+[assembly: AssemblyProduct("HidLogger")]
+[assembly: AssemblyCopyright("Copyright © MSDN.WhiteKnight 2018")]
+[assembly: AssemblyTrademark("MSDN.WhiteKnight")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("805e66ce-8f11-4252-b582-19a21f4e079a")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
